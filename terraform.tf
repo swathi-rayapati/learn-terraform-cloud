376c05108cabc4a2dcc9cf661c7323c32afd3157
swathi-rayapati <swathiakula2012@gmail.com>
2023-09-13 09:36:05 -0700

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,13 +1,13 @@
 terraform {
 
-  cloud {
-    organization = "organization-name"
+/*   cloud {
+    organization = "Org-tf-009"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
-
+ */
   required_providers {
     aws = {
       source  = "hashicorp/aws"
